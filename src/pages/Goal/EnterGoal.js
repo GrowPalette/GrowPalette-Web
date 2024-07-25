fd98020fb307c165b3edf7c1ed1f5bc170a3fa28
hyunwoo <hwjae1004@naver.com>
2024-07-25 23:35:12 +0900

    feat: 컨테이네 줄까지

--- src/pages/Goal/EnterGoal.js
+++ src/pages/Goal/EnterGoal.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import backIcon from "../../assets/images/back.svg"; // 이미지 경로를 올바르게 임포트
-
+import categoryIcon from "../../assets/images/categoryIcon.svg";
 const EnterGoal = () => {
   return (
     <Container>
@@ -9,6 +9,10 @@ const EnterGoal = () => {
         <StyledBackIcon src={backIcon} alt="뒤로가기" />
         <BackText>Back</BackText>
       </BackContainer>
+      <CategoryContainer>
+        <StyledCategoryIcon src={categoryIcon} alt="카테고리 아이콘" />
+        <CategoryText>카테고리</CategoryText>
+      </CategoryContainer>
     </Container>
   );
 };
@@ -37,4 +41,24 @@ const StyledBackIcon = styled.img`
 
 const BackText = styled.span`
   font-size: 16px; // 필요에 따라 텍스트 크기 조절
+`;
+
+const CategoryContainer = styled.div`
+  display: flex;
+  align-items: center; // 이미지와 텍스트를 수직 정렬
+  position: absolute;
+  top: 110px; // Figma 디자인에서 제공한 값으로 설정 (BackContainer 아래)
+  left: 24px; // Figma 디자인에서 제공한 값으로 설정
+  gap: 12px; // 이미지와 텍스트 사이의 간격
+`;
+
+const StyledCategoryIcon = styled.img`
+  width: 72px; // 필요에 따라 이미지 크기 조절
+  height: 24px; // 필요에 따라 이미지 크기 조절
+`;
+
+const CategoryText = styled.span`
+  font-size: 16px; // 텍스트 크기
+  font-weight: bold; // 텍스트를 볼드체로
+  // 또는 font-weight: 700; // 또는 숫자를 사용하여 볼드체로 설정
 `;
